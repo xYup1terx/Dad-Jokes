30e4b31cffa4ffeb6a62357700da1e901b0cfac9
Amarea <Amarea1103@gmail.com>
2026-02-21 13:13:45 -0500

    Replace innerHTML with textContent, Implement eventListener on button)

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ async function generateJoke() {
 
 const res = await fetch('https://icanhazdadjoke.com', config);
 const data = await res.json();
-jokeEl.innerHTML = data.joke;
+jokeEl.textContent = data.joke;
 }
 
 generateJoke();
+
+//listeners
+
+jokeBtn.addEventListener('click', generateJoke);
