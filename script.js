0ffd212ece874dd498041c6c78046d2557ee291e
Amarea <Amarea1103@gmail.com>
2026-02-21 13:07:22 -0500

    Implement generateJoke()

---
+++ script.js
@@ -0,0 +1,17 @@
+//selectors
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+async function generateJoke() {
+   const config = {
+    headers: {
+        Accept: 'application/json',
+    },
+   };
+
+const res = await fetch('https://icanhazdadjoke.com', config);
+const data = await res.json();
+jokeEl.innerHTML = data.joke;
+}
+
+generateJoke();
